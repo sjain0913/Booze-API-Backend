c10bc92b7d29c967139102c8115f51afc34f6a62
Saumya Jain <samjain0913@gmail.com>
2020-05-15 02:19:37 +0530

    restructure

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-var express = require('express');
-var app = express();
-module.export = app;
+const Express = require('express');
+const BodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+app.listen(5000, () => {});
--- server.js
+++
@@ -1,7 +0,0 @@
-// server side
-
-var app = require('./app');
-var port = process.env.PORT || 3000;
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
