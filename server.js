ba17598a3295985f6449e191dc8428e71785584a
Saumya Jain <samjain0913@gmail.com>
2020-05-15 00:05:19 +0530

    setting up the mongodb server

---
+++ server.js
@@ -0,0 +1,7 @@
+// server side
+
+var app = require('./app');
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
