40a3b44861a9defc55e2708f092f7dcc40313224
Saumya Jain <samjain0913@gmail.com>
2020-05-15 23:33:48 +0530

    added methods to post, delete, get specific and update

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-require('dotenv/config');
-const postsRoute = require('./routes/posts');
 
+const postsRoute = require('./routes/posts.js');
 const express = require('express');
-
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 
+require('dotenv/config');
+
 // Gives us the ability to create routes
 const app = express();
+app.use(bodyParser.json());
 
 // Middlewares: Function that executes when routes are being hit, can be used for authentication
 // can use to guide to different routes
 app.use('/posts', postsRoute);
+
+postsRoute.get('/', (req, res) => {
+    res.send("We are on posts");
+})
 
 // Connect to db
 mongoose.connect(
