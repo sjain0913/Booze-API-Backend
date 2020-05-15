b5e6c325a18b818d300cceed6cc35fd8dd7a73bc
Saumya Jain <samjain0913@gmail.com>
2020-05-15 17:32:55 +0530

    some changes to fix the paths

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const Express = require('express');
 const BodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require("mongodb").ObjectID;
+const URL = require('./connect');
+const NAME = "AlcoholAPI"
 
+var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
+var database, collection;
 
-app.listen(5000, () => {});
+app.listen(3000, () => {
+    MongoClient.connect(URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(NAME);
+        collection = database.collection("personnel");
+        console.log("Connected to `" + NAME + "`!");
+    });
+});
