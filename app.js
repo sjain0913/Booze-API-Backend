4075dfbc1537b231e29dfcc7bafd317216541349
Saumya Jain <samjain0913@gmail.com>
2020-05-16 12:57:33 +0530

    transforming to alchol data

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-// http://localhost:3000/posts
-
-const postsRoute = require('./routes/posts.js');
+// Link: http://localhost:3000/posts
+// Requiring modules
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-//remove cors to disallow cross domain access
-const cors = require('cors');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
+// Requiring routes
+const postsRoute = require('./routes/posts.js');
 
-// Gives us the ability to create routes
 const app = express();
-app.use(cors());
+// app.use(cors());      <== Uncomment when testing frontend (bypasses the crossdomain lock for accessing APIs)
 app.use(bodyParser.json());
 
-
-// Middlewares: Function that executes when routes are being hit, can be used for authentication
-// can use to guide to different routes
-app.use('/posts', postsRoute);
+// Routes
+app.use('/beer', beerRoute);
+app.use('/cider', ciderRoute);
+app.use('/mead', meadRoute);
+app.use('/vodka', vodkaRoute);
+app.use('/whiskey', whiskeyRoute);
+app.use('/gin', ginRoute);
+app.use('/brandy', brandyRoute);
+app.use('/rum', rumrRoute);
+app.use('/tequila', tequilaRoute);
+app.use('/absinthe', absintheRoute);
+app.use('/everclear', everclearRoute);
 
 postsRoute.get('/', (req, res) => {
     res.send("We are on posts");
