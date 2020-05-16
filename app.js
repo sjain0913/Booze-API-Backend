951975b98aa6d4b60e14efe655f44b859068dc48
Saumya Jain <samjain0913@gmail.com>
2020-05-16 13:04:48 +0530

    route files for different drinks

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Link: http://localhost:3000/posts
+
 // Requiring modules
 const express = require('express');
 const mongoose = require('mongoose');
@@ -9,7 +10,17 @@ const cors = require('cors');
 require('dotenv/config');
 
 // Requiring routes
-const postsRoute = require('./routes/posts.js');
+const postsRoute = require('./routes/beer.js');
+const postsRoute = require('./routes/cider.js');
+const postsRoute = require('./routes/mead.js');
+const postsRoute = require('./routes/vodka.js');
+const postsRoute = require('./routes/whiskey.js');
+const postsRoute = require('./routes/gin.js');
+const postsRoute = require('./routes/brandy.js');
+const postsRoute = require('./routes/rum.js');
+const postsRoute = require('./routes/tequila.js');
+const postsRoute = require('./routes/absinthe.js');
+const postsRoute = require('./routes/everclear.js');
 
 const app = express();
 // app.use(cors());      <== Uncomment when testing frontend (bypasses the crossdomain lock for accessing APIs)
@@ -28,15 +39,14 @@ app.use('/tequila', tequilaRoute);
 app.use('/absinthe', absintheRoute);
 app.use('/everclear', everclearRoute);
 
-postsRoute.get('/', (req, res) => {
-    res.send("We are on posts");
+postsRoute.get('/beer', (req, res) => {
+    res.send("We are on beer");
 })
 
-// Connect to db
+// Check if connected to MongoDB
 mongoose.connect(
     process.env.DB_CONNECTION, () => {
     console.log("connection to db works!");
 });
 
-// How to start listening to server
 app.listen(3000);
