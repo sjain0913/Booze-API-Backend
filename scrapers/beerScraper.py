65566a9fb87740a697c6aa7f6be04e23a8fd4c01
Saumya Jain <samjain0913@gmail.com>
2020-05-18 22:06:20 +0530

    selenium scraper

---
+++ scrapers/beerScraper.py
@@ -0,0 +1,7 @@
+from selenium.webdriver import Chrome
+import pandas as pd
+
+driverPath = "C:\Users\Sam\Documents\chromedriver.exe"
+url = "https://www.saucey.com/beer"
+
+driver = Chrome(driverPath)
