544cf186894cc27056434d69470510b98583d72a
Saumya Jain <samjain0913@gmail.com>
2020-05-24 12:50:52 +0530

    all json uploaded

--- app.js
+++ app.js
@@ -12,21 +12,13 @@ require('dotenv/config');
 
 // Requiring routes
 const beerRoute = require('./routes/beer.js');
-// const beerScraper = require('./scrapers/Beer/beerScraper.js');
 const vodkaRoute = require('./routes/vodka.js');
-// const vodkaScraper = require('./scrapers/Vodka/vodkaScraper.js');
 const whiskeyRoute = require('./routes/whiskey.js');
-// const whiskeyScraper = require('./scrapers/Whiskey/whiskeyScraper.js');
 const ginRoute = require('./routes/gin.js');
-// const ginScraper = require('./scrapers/Gin/ginScraper.js');
 const brandyRoute = require('./routes/brandy.js');
-// const brandyScraper = require('./scrapers/Brandy/brandyScraper.js');
 const rumRoute = require('./routes/rum.js');
-// const rumScraper = require('./scrapers/Rum/rumScraper.js');
 const tequilaRoute = require('./routes/tequila.js');
-// const tequilaScraper = require('./scrapers/Tequila/tequilaScraper.js');
 const liqueurRoute = require('./routes/liqueur.js');
-// const liqueurScraper = require('./scrapers/Liqueur/liqueurScraper.js');
 
 const app = express();
 // app.use(cors());      // <== Uncomment when testing frontend (bypasses the crossdomain lock for accessing APIs)
