f33062da999c2b96750774ae386f46ceaa2cd58f
Saumya Jain <samjain0913@gmail.com>
2020-05-26 13:04:22 +0530

    all schemas, data fixed

--- models/Beer.js
+++ models/Beer.js
@@ -11,6 +11,56 @@ const BeerSchema = mongoose.Schema({
         required: true,
         default: "0%"
     },
+    kind: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    maker: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    prices: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    city: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    region: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    country: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    continent: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    IBU: {
+        type: String,
+        required: true,
+        default: "IBUNotFound"
+    },
+    calories: {
+        type: String,
+        required: true,
+        default: "CaloriesNotFound"
+    },
+    carbohydrates: {
+        type: String,
+        required: true,
+        default: "CarbohydratesNotFound"
+    }
 });
 
 module.exports = mongoose.model('Beer', BeerSchema, 'Beer');
--- models/Vodka.js
+++ models/Vodka.js
@@ -11,6 +11,41 @@ const VodkaSchema = mongoose.Schema({
         required: true,
         default: "0%"
     },
+    kind: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    maker: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    prices: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    city: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    region: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    country: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    },
+    continent: {
+        type: String,
+        required: true,
+        default: "Not Defined"
+    }
 });
 
 module.exports = mongoose.model('Vodka', VodkaSchema, 'Vodka');
